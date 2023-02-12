92e4b462dbd578fe218107a5eb3e4ef4d069bb10
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 13:37:34 -0500

    Switch conditional

---
+++ 11-switch.js
@@ -0,0 +1,13 @@
+
+var num = 1;
+
+switch(num){
+    case 1:
+        console.log("I am 1");
+        break;
+    case 10:
+        console.log("I am 10");
+        break;
+    default:
+        console.log("I am not");
+}
