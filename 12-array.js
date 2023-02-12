3a69cbe10e5e11bae4fc46791476cb198130eb17
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 13:39:10 -0500

    Array methods push, pop, unshift, shift and indexOf

---
+++ 12-array.js
@@ -0,0 +1,25 @@
+//data structure
+var fruits = ['apple','orange','banana'];
+console.log(fruits);
+
+//length
+fruits.length;
+
+//by item
+fruits[0];
+
+//methods
+
+//adds items into the last position
+fruits.push('grapes','pineapple');
+//removes the last item
+fruits.pop();
+//adds items into the first position
+fruits.unshift('pear','bananas');
+//removes the first item
+fruits.shift();
+//returns item position
+fruits.indexOf('apple');
+
+
+
