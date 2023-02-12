e2f174acb3daa1850309cf41f2e0a5b9d2850585
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 13:26:56 -0500

    Operators

---
+++ 8-operators.js
@@ -0,0 +1,41 @@
+// operation or binaries operators
+3 + 2
+50 - 10
+10 * 20
+20 / 2
+"Jhojan " + "Tobon" //concatenates
+
+//unitary operator
+!false  //result true 
+
+//assignation operator
+var a = 1; 
+
+//comparison operators
+3 == "3"; //compares only the value
+//result: true
+3 === "3" //compares both value and type
+
+5 < 3; 
+//result: false
+5 > 3;
+//result: true
+5 <= 6;
+//result: true
+5 >= 6;
+//result: false
+
+true && true
+
+true || false
+
+//incremental
+var edad = 30;
+edad++;
+
+edad +=2;
+
+
+
+
+
