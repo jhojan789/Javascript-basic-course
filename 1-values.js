a2ca7d74c56dc253cd076fccf0a8cd31b6062bcc
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 12:16:05 -0500

    Value types in javascript

---
+++ 1-values.js
@@ -0,0 +1,19 @@
+//primitive values
+//numeric
+40
+//text
+'Jhojan Tobón'
+"Jhojan Tobón"
+//boolean by George Boolean
+true
+false
+//empty values or placeholder values
+null
+undefined
+//non-primitive values or object type
+//array
+[1,2,3,4]
+//Object
+{name:"Jhojan"}
+
+//typeof 40 to see the type of the value.
