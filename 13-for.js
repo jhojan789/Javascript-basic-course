9e170861e71b03220d4ba7d62e0ba778baea4019
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 13:39:44 -0500

    For loop

---
+++ 13-for.js
@@ -0,0 +1,10 @@
+let students = ['Maria','Sergio','Jose'];
+
+function greetings(student) {
+    console.log(`Hello ${student}`);
+}
+
+for (let i = 0; i < students.length; i++) {
+    greetings(students[i]);
+    
+}
