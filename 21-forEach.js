e4e00eb49281a5964435501c47e84b481d7bc1c6
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 14:03:59 -0500

    Method to go through an array - forEach

---
+++ 21-forEach.js
@@ -0,0 +1,14 @@
+var items = [
+    { name: "Bici", cost: 3000 },
+    { name: "TV", cost: 2500 },
+    { name: "Libro", cost: 320 },
+    { name: "Celular", cost: 10000 },
+    { name: "Laptop", cost: 20000 },
+    { name: "Teclado", cost: 500 },
+    { name: "Audifonos", cost: 1700 },
+  ];
+
+
+items.forEach(function(item){
+    console.log(item.name);
+});
