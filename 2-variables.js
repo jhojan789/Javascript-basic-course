823ccd94fc760d009ca4c803e3499a3f9653c9fb
Jhojan Tobón <jhojantobonm@gmail.com>
2023-02-12 12:31:30 -0500

    How to declare and initialize variables var,let,const

---
+++ 2-variables.js
@@ -0,0 +1,25 @@
+var name = 'Jhojan';
+let name2 = 'Tobón';
+const name3 = 'M';
+
+//declare a variable
+let age;
+//initialize a variable
+age = 30;
+//to call
+age;
+
+//array
+let elements = [1,'mouse'];
+//to call
+elements[1];
+
+
+//object
+let person={
+    name4:'Jhojan',
+    age2: 30
+}
+//to call
+person.name4;
+
